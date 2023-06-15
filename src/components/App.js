d1a3afd22b7e59a60a591845bdef4b3a29f2b0bb
skfarukhossain1998 <skfarukhossain24u@gmail.com>
2023-06-15 16:39:21 +0530

    1st attempt

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tab from './Tab'
+
+const tabs=[{title:"Tab 1"}, {title:"Tab 2"}, {title:"Tab 3"}]
+
+
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Tab tabs={tabs}/>
     </div>
   )
 }
---
+++ src/components/Tab.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+const Tab=({tabs})=>{
+const [tabClick,setTabClick]=useState("");
+/* function handleCheck(){
+        setTabClick(tabs.title)
+} */
+
+
+
+
+return (
+<div>
+    <ul>
+        {
+            tabs.map((tab)=>(
+                <li
+                onClick={()=>setTabClick(tab.title)}
+                >{tab.title}</li>
+            ))
+        }
+
+     </ul>
+    {
+    tabClick &&
+    <p>This is content for {tabClick}</p>
+    
+    }
+
+</div>
+
+)
+}
+export default Tab;
+
